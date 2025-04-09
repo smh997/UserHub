8254760a1edfe2d5e46fa68c3efba004cbf9fe02
smh10 <smh10.mh@gmail.com>
2025-04-09 16:03:27 -0400

    Add User data model

---
+++ app/src/main/java/com/example/userhub/data/User.kt
@@ -0,0 +1,9 @@
+package com.example.userhub.data
+
+data class User(
+    val uid: String = "",
+    val email: String = "",
+    val displayName: String = "",
+    val profileImageUrl: String = ""
+)
+
